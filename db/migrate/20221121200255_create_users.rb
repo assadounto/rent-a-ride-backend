0f3123ad927bf363022e71725893d20c342588e2
rich <adukyerer@gmail.com>
2022-12-18 13:00:58 +0000

    Add role

--- db/migrate/20221121200255_create_users.rb
+++ db/migrate/20221121200255_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration[7.0]
       t.string :name
       t.string :username
       t.string :email
+      t.string :role ,default: "user"
       t.string :password_digest
       t.timestamps
     end
